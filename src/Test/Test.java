9c8b4528c1cade9e260caab31563209e6ec1be50
Bad-Snowman <rasmusls98@gmail.com>
2018-09-25 10:51:18 +0200

    Test

--- src/Test/Test.java
+++ src/Test/Test.java
@@ -2,7 +2,6 @@ package Test;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Test1");
-        System.out.println("Test2");
+                System.out.println("Test2");
     }
 }
